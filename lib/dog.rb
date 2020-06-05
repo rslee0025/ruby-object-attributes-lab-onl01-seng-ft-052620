832f959ee13ed0b7231d74d319d29ca0bf18098e
Rachel S. Lee <rslee0025@gmail.com>
2020-06-05 01:05:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,4 @@
 class Dog 
-  def initialize(name)
-    @name = name 
-  end 
-  
-  def name 
-    @name 
-  end 
-  
-  def name= (new_name)
-    @name = "fido"
-  end 
+
 end
 
-fido = Dog.new
-fido.name = "fido"
