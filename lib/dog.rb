3742c1a7176feec3ad0a7069fea9fc4e7977a9a8
Rachel S. Lee <rslee0025@gmail.com>
2020-06-05 01:07:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,11 @@
 class Dog 
 
-def name= (dogs_name)
-  @name = dogs_name
-end  
+  def name= (dogs_name)
+    @name = dogs_name
+  end  
 
-def name = 
-
-
-
-
-
+  def name
+    @name
+  end 
 end
 
